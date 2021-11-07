172128c1cb2f94aa32e48b6d406d4d69f1c1f870
Vladimir1506 <47722156+Vladimir1506@users.noreply.github.com>
2021-11-07 20:39:34 +0300

    Add files via upload

---
+++ independentWork-1/Проверка логина.js
@@ -0,0 +1,13 @@
+let login = prompt('Введите ваш логин','')
+if (login=='Админ'){
+	let password = prompt('Введите ваш пароль')
+	if (password=='Я главный'){
+	alert('Здравствуйте!')
+	} else if (password==null){
+	alert('Отменено')
+	} else 
+	alert('Неверный пароль')
+} else if (login==''||login==null){
+	alert('Отменено')
+} else 
+alert('Я вас не знаю')
