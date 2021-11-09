d2f2fa7f52bbe07ddd9ac2b4d72ec715176eb348
Vladimir1506 <47722156+Vladimir1506@users.noreply.github.com>
2021-11-09 21:10:13 +0300

    Add files via upload

---
+++ independentWork-1/classWork.js
@@ -0,0 +1,7 @@
+export const getHolidayPrizes = (fio, date, sum) => {
+  let today = new Date();
+ let dateBirth= new Date(Date.parse(date)) ;
+if (today.getDate()==dateBirth.getDate()&&today.getMonth()==dateBirth.getMonth()){
+   return `У ${fio} сегодня праздник, его премия составляет ${sum}`;
+ } else return today.getMonth()+" "+dateBirth.getMonth()
+};
