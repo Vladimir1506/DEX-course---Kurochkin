9b737ec985cf9f3b911d2a964421ae5b8e402022
Vladimir1506 <47722156+Vladimir1506@users.noreply.github.com>
2021-11-09 21:16:03 +0300

    Update classWork.js

--- independentWork-1/classWork.js
+++ independentWork-1/classWork.js
@@ -3,5 +3,5 @@ export const getHolidayPrizes = (fio, date, sum) => {
  let dateBirth= new Date(Date.parse(date)) ;
 if (today.getDate()==dateBirth.getDate()&&today.getMonth()==dateBirth.getMonth()){
    return `У ${fio} сегодня праздник, его премия составляет ${(Math.round((sum*0.1+today.getDate()*today.getMonth())/10))*10}`;
- } else return today.getMonth()+" "+dateBirth.getMonth()
+ } 
 };
