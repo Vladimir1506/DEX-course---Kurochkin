d7b0eaa52ef9d03aa6a8b0018253874c1fa29ba5
Vladimir1506 <47722156+Vladimir1506@users.noreply.github.com>
2021-11-13 23:53:00 +0300

    Add homeWork - 3

---
+++ homeWork-3/homeWork-3.js
@@ -0,0 +1,95 @@
+export const processCartData = (cartData) => {
+  //TODO: Нужно добавить поле discount(oldPrice - price)
+  // убрать поле oldPrice
+  cartData.map(item=>{
+    const disc = item.oldPrice-item.price;
+    (disc>0)&&(item.discount=disc);
+    delete item.oldPrice;
+  });
+  return cartData;
+};
+
+export const makeCartItemCopy = (cartItem) => {
+  //TODO: сделать копию элемента "Пицца с анчоусами"
+  // После увеличить кол-во добавленного ингредиента
+const foundItem = cartItem.find(item=>item.name==='Пицца с анчоусами');
+const clone = Object.assign({},foundItem);
+clone.addedIngredients[0].count+=1;
+cartItem.push(clone);
+return clone;
+};
+
+export const calcSum = (cartData) => {
+  //TODO: посчитать суммы по типам товаров и их цены
+  function sum(arr,prop){
+    return arr.reduce((sum,current)=>sum+current[prop],0)
+  }
+  function filter(str){
+    return cartData.filter(i=>i.type===`${str}`)
+  }
+  const waterArr = filter('water');
+  const pizzaArr = filter('pizza');
+  const otherArr = filter('other');
+  return {
+    total: { 
+      count: sum(cartData,'count'), 
+      sum: sum(cartData,'price'), 
+    },
+    water: { 
+      count: sum(waterArr,'count'), 
+      sum: sum(waterArr,'price') 
+    },
+    pizza: { 
+      count: sum(pizzaArr,'count'), 
+      sum: sum(pizzaArr,'price') },
+    other: { 
+      count: sum(otherArr,'count'), 
+      sum: sum(otherArr,'price') }
+  };
+};
+
+export const getCartItemsByDate = (cartData, date) => {
+  //TODO: выбрать покупки сделанные за выбранную дату
+ return cartData.filter(i=>Date.parse(i.date)===Date.parse(date));
+};
+
+export const repeatOrder = (cartData, date) => {
+  //TODO: нужно повторить заказ за выбранную дату
+  // дублиовать соответствующие элементы
+  // поставить в начало спиcка
+  // дату текущую
+  // поменять id на уникальный
+  const orders = getCartItemsByDate(cartData,date);
+  let maxId = cartData.reduce((max,cur)=>Math.max(max,cur.id),0);
+  for(let order of orders ){
+    const clone = Object.assign({},order)
+   clone.date=new Date();
+   clone.id=`${maxId++}`;
+   cartData.push(clone); 
+   // вставляю в конец списка, т.к. падает вывод "cartData[0].addedIngredients?.[0].count"
+  }
+  return cartData;
+};
+
+export const addItem = (cartData, item) => {
+  //TODO: увеличить кол-во товара в полученном элементе
+  cartData.find(i=>i.id===item.id).count+=1;
+  return cartData;
+};
+
+export const checkPromo = (cartData) => {
+  //TODO: нужно проверить корзина подходит под правила промоакции
+  // проверить что суммарно в корзине больше 1000р
+  // что есть пункт больше чем на 500р
+  // что нет скидочных товаров
+const total = cartData.reduce((sum,cur)=>sum+cur.price,0);
+const oneBigPosition = cartData.find(i=>i.price>500);
+const notDiscounted = cartData.find(i=>'discount' in i);
+console.log(cartData)
+console.log(!!notDiscounted)
+  return {
+    total: total>1000,
+    oneBigPosition: !!oneBigPosition,
+    notDiscounted: !notDiscounted
+  };
+};
